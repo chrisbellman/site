afd4b9ea508346249bc068cd87cb920d26cc4aab
Christian Bellman <4149988+chrisbellman@users.noreply.github.com>
2023-09-08 20:55:35 -0700

    Initial commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,9 +6,6 @@ const blogSchema = z.object({
     updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
     badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
 });
 
 const storeSchema = z.object({
@@ -18,7 +15,7 @@ const storeSchema = z.object({
     custom_link: z.string().optional(),
     updatedDate: z.coerce.date(),
     pricing: z.string().optional(),
-    oldPricing: z.string().optional(),
+    oldPricing:  z.string().optional(),
     badge: z.string().optional(),
     checkoutUrl: z.string().optional(),
     heroImage: z.string().optional(),
